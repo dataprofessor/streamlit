9cfda8f60efbb72f0ea013b0d67c17eae8673e5b
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-02-07 16:47:25 +0700

    Update selectbox_app.py

--- selectbox_app.py
+++ selectbox_app.py
@@ -13,6 +13,11 @@ contact_selected = st.selectbox("How would you like to be contacted?",
 st.write("Selectbox returns:", contact_selected,
          "of type", type(contact_selected))
 
+if contact_selected == "Email":
+    st.write("**Comfirm your email address by clicking the link sent to you**")
+else:
+    st.write("**Thank you, we will be in touch soon**")
+
 st.header("Selectbox from a NumPy array")
 
 array = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
